597fcfbe8feea02554a96926c34576e6ba3ccf3d
Jessica Stone <jstone4815@gmail.com>
2020-06-23 10:08:41 -0700

    add Movies to page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,26 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+import MovieGrid from './MovieGrid';
+
 const Search = (props) => {
   const BASE_URL = "http://localhost:3000/";
   
+  const [query, setQuery] = useState({});
+  const [movies, setMovies] = useState([]);
+
+  // let movieComponents = [];
+    
   const onSearch = query => {    
     axios.get(BASE_URL + "movies?query=" + query)
       .then((response) => {
         if (response.data.length === 0) {
           console.log("no movies found");
         } else {
-          const movies = response.data.map(movie => {
-            console.log("movie title: " + movie.title);
-            return movie.title;
+          let moviesCopy = response.data.map(movie => {
+            return movie;
           });
+          setMovies(moviesCopy);
         }
       })
       .catch((error) => {
@@ -22,9 +29,7 @@ const Search = (props) => {
         console.log("failed search: " + error);
       })    
     }
-
-  const [query, setQuery] = useState({});
-
+  
   const onInputChange = event => {
     const queryCopy = query;
     queryCopy[event.target.name] = event.target.value;
@@ -48,21 +53,25 @@ const Search = (props) => {
   
   return (
     // after making search, can add a movie from results to rental library (local API)
-    <form className="" onSubmit={onSubmit}>
-    <header className="">Search the Database</header>
-    <div className="">
-      <input
-        className=""
-        type="text"
-        name="text"
-        placeholder="search here"
-        value={query.text}
-        onChange={onInputChange}
+    <div>
+      <form className="" onSubmit={onSubmit}>
+      <header className="">Search the Database</header>
+        <div className="">
+          <input
+            className=""
+            type="text"
+            name="text"
+            placeholder="search here"
+            value={query.text}
+            onChange={onInputChange}
+          />
+          <input type="submit" value="Search" className="" onSubmit={onSubmit}/>
+        </div>
+      </form>
+      <MovieGrid
+        moviesList={movies}
       />
-      <input type="submit" value="Search" className="" onSubmit={onSubmit}/>
-  </div>
-</form>
-)
-}
+    </div>
+  )}
 
 export default Search
