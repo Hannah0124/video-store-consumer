648bef3b1dc881f2b30c15d36f9f76edaf3c8c24
Hannah <hannahjoo24@gmail.com>
2020-06-23 09:37:17 -0700

    Update '/customers' route in Customers and Customer.js

---
+++ src/components/Customer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+
+const Customer = (props) => {
+  return (
+    <div className={props.id}>
+      <img src="https://placedog.net/300/300?random" alt="dog" />
+      <h1>{props.name}</h1>
+
+      <div>
+        <p>{props.address}</p>
+        <p>{props.state}, {props.postalCode}</p>
+        <p>{props.phone}</p>
+        <p><strong>Movies Checked Out: </strong>{props.moviesCheckedOutCount}</p>
+      </div>
+
+      <button>Select Customer</button>
+    </div>
+  );
+};
+
+
+// // TODO
+// Customer.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   imageUrl: PropTypes.string,
+//   overview: PropTypes.string,
+//   releaseDate: PropTypes.string,  
+//   title: PropTypes.string.isRequired
+// };
+
+export default Customer;
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,13 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Customer from './Customer';
 
-const Customers = () => {
+
+const Customers = (props) => {
+
+  const[customers, setCustomers] = useState([]);
+  const[errorMessage, setErrorMessage] = useState(null); 
+
+  useEffect(() => {
+    axios.get(props.baseUrl + "customers/")
+    
+    .then((response) => {
+      const apiData = response.data;
+      console.log("customers:", apiData);
+
+      const customerObjects = apiData.map((customer, i) => {
+        console.log("customer: ", customer);
+
+        return {
+          id: customer.id,
+          name: customer.name,
+          accountCredit: customer.account_credit,
+          address: customer.address,
+          moviesCheckedOutCount: customer.movies_checked_out_count,
+          phone: customer.phone,
+          postalCode: customer.postal_code,
+          registeredAt: customer.registered_at,
+          state: customer.state
+        }
+      });
+
+      setCustomers(customerObjects);
+
+      })
+
+      .catch((error) => {
+        // console.log("error: ", error.message)
+        setErrorMessage(error.message);
+      });
+  }, []);
+
+
+  const customerComponents = customers.map((customer, i) => {
+    return (
+      <section key={customer.id}>
+        {errorMessage &&
+        <div className="validation-errors-display__list">
+          <h2>{errorMessage}</h2>
+        </div>}
+
+        <Customer 
+          id={customer.id}
+          name={customer.name}
+          accountCredit={customer.account_credit}
+          moviesCheckedOutCount={customer.movies_checked_out_count}
+          phone={customer.phone}
+          postalCode={customer.postal_code}
+          registeredAt={customer.registered_at}
+          state={customer.state}
+        />
+      </section>
+    );
+  });
+
   return (
     // can see a list of all customers - make a call to the internal video store API?
 
     // can select a customer
     // selection will then be visible across the app - use a state to store this info?
-    <div>This is the Customers page.</div>
-  )
-}
+    <div>
+      <h1>Customers</h1>
+      <div className="customer">{customerComponents}</div>
+    </div>
+  );
+};
 
-export default Customers
+export default Customers;
