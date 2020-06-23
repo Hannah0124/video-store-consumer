a2d6686b4254bda2d6ba12e1e040e4cddee79ee4
Hannah <hannahjoo24@gmail.com>
2020-06-22 20:47:52 -0700

    Test axios call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 // import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
-import './App.css';
 
+import './App.css';
 import Home from './components/Home';
 import Search from './components/Search';
 import Library from './components/Library';
@@ -10,6 +13,33 @@ import Customers from './components/Customers';
 
 
 const App = () => {
+  // const[cards, setCards] = useState([]);
+  // const[errorMessage, setErrorMessage] = useState(null);
+  // BASE_URL = "http://localhost:3000/movies/Psycho";
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/movies/Psycho")
+    .then((response) => {
+      // const apiData = response.data;
+
+      console.log("hey", response)
+
+      // const cardObjects = apiData.map((cardWrapper, i) => {
+      //   return {
+      //     id: cardWrapper.card.id,
+      //     text: cardWrapper.card.text,
+      //     emoji: cardWrapper.card.emoji
+      //   }
+      // });
+      // setCards(cardObjects);
+      })
+      .catch((error) => {
+        console.log("error: ", error.message)
+        // setErrorMessage(error.message);
+      });
+  }, []);
+
   return (
     <Router>
       <nav>
@@ -28,18 +58,16 @@ const App = () => {
           </li>
         </ul>
       </nav>
-      {/* movie and customer selection will be visible across each page as you navigate the app */}
-      {/* if customer & movie are selected, can check out selected movie for the selected customer (checkout is between ONE movie and ONE customer) */}
 
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/library" component={Library} />
-        <Route exact path="/customers" component={Customers} />
+        <Route exact path="/"><Home/></Route>
+        <Route exact path="/search"><Search /></Route>
+        <Route exact path="/library"><Library /></Route>
+        <Route exact path="/customers"><Customers /></Route>
       </Switch>
     </Router>
-  );
-};
+  )
+}
 
 export default App;
 
