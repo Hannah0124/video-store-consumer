be3c2d8613c233740d2efd7da87d5a45ab7845e2
Jessica Stone <jstone4815@gmail.com>
2020-06-24 16:05:26 -0700

    fix card flip and remove toggle

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,29 +7,21 @@ import { createPopper } from '@popperjs/core';
 
 // selectMovieCallback={props.selectMovieCallback}
 const Movie = (props) => {
-
-  const[movieFront,setmovieFront] = useState("active");
-  const[movieBack,setmovieBack] = useState("inactive");
-  const[toggled,setToggled] = useState(false);
-
   const onSelectMovie = (event) => {
     return props.movieCallback({id: props.id, title: props.title, overview: props.overview, imageURL: props.imageUrl, releaseDate: props.releaseDate});
   };
 
-  const toggleView = () => {
-    (movieFront === "active") ? setmovieFront("inactive") : setmovieFront("active");
-    (movieBack === "active") ? setmovieBack("inactive") : setmovieBack("active");
-  }
-
   return (
-    <div className="movie-card p-3 col-3 text-center align-self-center" key={props.id} onMouseEnter={toggleView} onMouseLeave={toggleView}>
-      <div className="flipper">
-        <div className={movieFront + " movie-front col-12"}>
-          <img src={props.imageUrl} alt={props.title} className="movie-poster mb-3" /> 
-          <h3 className="movie-title">{props.title}</h3>
-          <p className="release-date"><strong>Released</strong>: {props.releaseDate}</p>
+    <div class="movie-container col-3 text-center mb-3 p-3">
+      <div class="flipper">
+        <div class="front">
+          <div className="">
+            <img src={props.imageUrl} alt={props.title} className="movie-poster mb-3" /> 
+            <h3 className="movie-title">{props.title}</h3>
+            <p className="release-date"><strong>Released</strong>: {props.releaseDate}</p>
+          </div>
         </div>
-        <div className={movieBack + " movie-back col-12 align-self-center p-3"}>
+        <div class="back p-4 d-flex flex-column">
           <p className="movie-overview">{props.overview}</p>
           <button className="button-bg" onClick={onSelectMovie}>{props.buttonText}</button>
         </div>
