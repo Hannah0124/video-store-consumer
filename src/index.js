6bc053decb1391c5727c08bae86fe1df4fa8f526
Hannah <hannahjoo24@gmail.com>
2020-06-23 19:46:00 -0700

    Add bootstrap and button style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
