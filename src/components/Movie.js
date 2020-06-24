15692c3bb829732324522485fc91aea0c5743407
Jessica Stone <jstone4815@gmail.com>
2020-06-24 13:19:08 -0700

    add movie details toggle

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import './Movie.css';
@@ -6,16 +6,24 @@ import './Movie.css';
 // selectMovieCallback={props.selectMovieCallback}
 const Movie = (props) => {
 
+  const[moviePoster,setMoviePoster] = useState("active");
+  const[movieDetails,setMovieDetails] = useState("inactive");
+
   const onSelectMovie = (event) => {
     // console.log("event: ", event.target.value);
     return props.movieCallback({id: props.id, title: props.title, overview: props.overview, imageURL: props.imageUrl, releaseDate: props.releaseDate});
   };
 
+  const toggleView = () => {
+    (moviePoster === "active") ? setMoviePoster("inactive") : setMoviePoster("active");
+    (movieDetails === "active") ? setMovieDetails("inactive") : setMovieDetails("active");
+  }
+
 
   return (
     <div className="movie-container">
-      <img src={props.imageUrl} alt={props.title} className="movie-poster"/>
-      <div className="movie-info">
+      <img src={props.imageUrl} alt={props.title} className={"movie-poster " + moviePoster} onClick={toggleView}/>
+      <div className={"movie-info " + movieDetails} onClick={toggleView}>
         <h3>{props.title}</h3>
         <p className="release-date">Released Date: {props.releaseDate}</p>
         <p>{props.overview}</p>
@@ -24,6 +32,19 @@ const Movie = (props) => {
     </div>
   )
 }
+  
+//   return (
+//     <div className="movie-container">
+//       <img src={props.imageUrl} alt={props.title} className="movie-poster"/>
+//       <div className="movie-info">
+//         <h3>{props.title}</h3>
+//         <p className="release-date">Released Date: {props.releaseDate}</p>
+//         <p>{props.overview}</p>
+//         <button className="button-bg" onClick={onSelectMovie}>{props.buttonText}</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 
 // // TODO
