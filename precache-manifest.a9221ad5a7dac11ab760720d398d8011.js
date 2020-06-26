35e88f38701068a15db457979335d3f3841db035
Hannah <hannahjoo24@gmail.com>
2020-06-25 22:43:39 -0700

    Updates

---
+++ precache-manifest.a9221ad5a7dac11ab760720d398d8011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f607e73545f80a6dd6ddf719b2efdf9e",
+    "url": "/video-store-consumer/index.html"
+  },
+  {
+    "revision": "5d12da25f50702614c20",
+    "url": "/video-store-consumer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e21eacd70074b7566022",
+    "url": "/video-store-consumer/static/css/main.1fd36ef2.chunk.css"
+  },
+  {
+    "revision": "5d12da25f50702614c20",
+    "url": "/video-store-consumer/static/js/2.b1e08b0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/video-store-consumer/static/js/2.b1e08b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21eacd70074b7566022",
+    "url": "/video-store-consumer/static/js/main.cc32453f.chunk.js"
+  },
+  {
+    "revision": "4cad2615cad4c20889c0",
+    "url": "/video-store-consumer/static/js/runtime-main.888f3c54.js"
+  },
+  {
+    "revision": "32fa3255a68bd9781a6748b280f6c63b",
+    "url": "/video-store-consumer/static/media/doge-rentals-logo.32fa3255.png"
+  }
+]);
